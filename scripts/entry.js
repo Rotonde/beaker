a8f3dae11b4252ea95d4db350053fdc70c159b3c
Josh Avanier <joshavanier@gmail.com>
2017-10-14 11:09:59 +0700

    Fix timestamp issue

--- scripts/entry.js
+++ scripts/entry.js
@@ -19,6 +19,7 @@ function Entry(data)
     var html = "";
 
     html += "<a href='"+this.dat+"'><img class='icon' src='"+this.dat+"/media/content/icon.svg'></a>";
+
     html += "<t class='portal'><a href='"+this.dat+"'>@"+this.portal+"</a>"+(this.target ? " > <a href='"+this.target+"'>"+(this.message.split(" ")[0])+"</a>" : "")+"</t>";
 
     if(this.portal == r.portal.data.name){
@@ -27,13 +28,12 @@ function Entry(data)
     else{
       html += this.editstamp ? "<c class='editstamp' data-operation='@"+this.portal+" '>edited "+timeSince(this.editstamp)+" ago</c>" : "<c class='timestamp' data-operation='@"+this.portal+" '>"+timeSince(this.timestamp)+" ago</c>";
     }
-    html += "<hr />"
+    html += "<hr />";
     html += "<t class='message'>"+(this.formatter(this.message))+"</t><br/>";
 
     if(this.media){
-      html += "<img class='media' src='"+this.dat+"/media/content/"+this.media+".jpg'/>"
+      html += "<img class='media' src='"+this.dat+"/media/content/"+this.media+".jpg'/>";
     }
-
     return "<div class='entry'>"+html+"<hr/></div>";
   }
 
@@ -68,6 +68,9 @@ function Entry(data)
       if(word.substr(0,6) == "dat://"){
         var compressed = word.substr(0,12)+".."+word.substr(word.length-3,2);
         n.push("<a href='"+word+"'>"+compressed+"</a>");
+      }
+      else if(word.substr(0,1) == "#"){
+        n.push("<c class='hashtag' data-operation='filter "+word+"'>"+word+"</c>");
       }
       else{
         n.push(word)
@@ -109,27 +112,28 @@ function timeSince(date)
   var interval = Math.floor(seconds / 31536000);
 
   if (interval >= 1) {
-    var years = interval == 1 ? " year" : " years"
+    var years = interval == 1 ? " year" : " years";
     return interval + years;
   }
   interval = Math.floor(seconds / 2592000);
   if (interval >= 1) {
-    var months = interval == 1 ? " month" : " months"
+    var months = interval == 1 ? " month" : " months";
     return interval + months;
   }
   interval = Math.floor(seconds / 86400);
   if (interval >= 1) {
-    var days = interval == 1 ? " day" : " days"
+    var days = interval == 1 ? " day" : " days";
     return interval + days;
   }
   interval = Math.floor(seconds / 3600);
   if (interval >= 1) {
-    var hours = interval == 1 ? " hour" : " hours"
+    var hours = interval == 1 ? " hour" : " hours";
     return interval + hours;
   }
   interval = Math.floor(seconds / 60);
   if (interval > 1) {
-    return interval + " minutes";
+    var minutes = interval == 1 ? " minute" : " minutes";
+    return interval + minutes;
   }
   return "seconds";
 }
