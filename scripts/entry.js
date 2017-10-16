2a34f68969b86aa9e3b220db9279ae179db1d894
Devine Lu Linvega <aliceffekt@gmail.com>
2017-10-16 20:42:20 +1300

    Fixed edit button

--- scripts/entry.js
+++ scripts/entry.js
@@ -24,7 +24,7 @@ function Entry(data)
     html += "<t class='portal'><a href='"+this.dat+"'>"+(this.seed ? "@" : "~")+this.portal+"</a>"+(this.target ? " > <a href='"+this.target+"'>"+(this.message.split(" ")[0])+"</a>" : "")+"</t>";
 
     if(this.portal == r.portal.data.name){
-      html += this.editstamp ? "<c class='editstamp' data-operation='"+(this.dat == r.portal.data.dat ? 'edit:'+this.id+' '+this.message.replace("'","") : '')+"'>edited "+timeSince(this.editstamp)+" ago</c>" : "<c class='timestamp' data-operation='edit:"+this.id+" "+this.message.replace("'","")+"'>"+timeSince(this.timestamp)+" ago</c>";
+      html += this.editstamp ? "<c class='editstamp' data-operation='"+('edit:'+this.id+' '+this.message.replace("'",""))+"'>edited "+timeSince(this.editstamp)+" ago</c>" : "<c class='timestamp' data-operation='edit:"+this.id+" "+this.message.replace("'","")+"'>"+timeSince(this.timestamp)+" ago</c>";
     }
     else{
       html += this.editstamp ? "<c class='editstamp'>edited "+timeSince(this.editstamp)+" ago</c>" : "<c class='timestamp'>"+timeSince(this.timestamp)+" ago</c>";
